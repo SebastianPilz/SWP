00179924f08b0fad74cdebc400d8fab60cc2dd79
Sebastian Pilz <SebastianPilz@Super-Mac-Luft.local>
2018-11-08 08:08:36 +0100

    hallo

--- PersonInterface/src/Main.java
+++ PersonInterface/src/Main.java
@@ -7,11 +7,17 @@ public class Main {
 		ArrayList<IBiggerThan> l = new ArrayList<>();
 		l.add(new Person("Martin", "Schoepf", 25));
 		l.add(new Person("Sebastian", "Pilz", 17));
+		l.add(new Person("Roshy", "Moon", 55));
+		l.add(new Person("Mario", "Bernardi", 60));
+		l.add(new Person("Lukas", "Heber", 99));
+		l.add(new Person("Alex", "Widerin", 18));
+		l.add(new Person("Elias", "Neuner", 8));
 		
-		sort(l);
+		System.out.println(sort(l));
 	}
 	
-	public static void sort(ArrayList<IBiggerThan> l) {
+	public static ArrayList<IBiggerThan> sort(ArrayList<IBiggerThan> a) {
+		/**
 		for (int i = 0; i < l.size() - 1; i++) {
 			if (l.get(i).isBiggerThan(l.get(i + 1))) {
 				System.out.println("is bigger");
@@ -19,5 +25,17 @@ public class Main {
 				System.out.println("is smaller");
 			}
 		}
+		**/
+		for (int j = 0; j < (a.size() * 3); j++) {
+			for (int i = 0; i < a.size() - 1; i++) {
+				if (a.get(i).isBiggerThan(a.get(i + 1))) {
+					IBiggerThan z = a.get(i);
+					a.remove(i);
+					a.add(z);
+				}
+			}
+		}
+
+		return a;
 	}
 }
