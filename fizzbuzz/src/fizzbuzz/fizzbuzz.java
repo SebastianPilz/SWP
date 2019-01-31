9db826a62ef62a3cdf51e793121ac9d5a4020f6c
Sebastian Pilz <SebastianPilz@Super-Mac-Luft.local>
2019-01-31 08:43:16 +0100

    hallo

--- fizzbuzz/src/fizzbuzz/fizzbuzz.java
+++ fizzbuzz/src/fizzbuzz/fizzbuzz.java
@@ -1,14 +1,16 @@
 package fizzbuzz;
 
 public class fizzbuzz {
-
+	// Wenn die Zahl durch 2 geteilt werden kann fizz
+	// Wenn die Zahl durch 3 geteilt werden kann buzz
+	// Wenn beides zutrifft fizzbuzz 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 100; i++) {
-			if (i % 3 == 0 && i % 4 != 0) {
-				System.out.printf("%d = fizz \n", i);
-			} else if (i % 4 == 0 && i % 3 != 0) {
+			if (i % 3 == 0 && i % 2 != 0) {
 				System.out.printf("%d = buzz \n", i);
-			} else if (i % 4 == 0 && i % 3 == 0) {
+			} else if (i % 2 == 0 && i % 3 != 0) {
+				System.out.printf("%d = fizz \n", i);
+			} else if (i % 2 == 0 && i % 3 == 0) {
 				System.out.printf("%d = fizzbuzz \n", i);
 			} else {
 				System.out.printf("%d = %d \n", i, i);
